e1ba9903fec0d582484e1d2706dd96a47b7a1b9f
sharma-kunal <kunalsharma6914@gmail.com>
2019-02-12 02:54:42 +0530

    Added some comments

--- desktopCleaner.py
+++ desktopCleaner.py
@@ -1,10 +1,15 @@
+
+# This code can be run as it is (just run it) on any Debian based distro.
+# But it can also be used to run on any other Operating System by just
+# changing the mypath variable to your desired folder which you want to
+# clean up.
 import os
 import shutil
 from os import listdir
 from os.path import isfile, join
 mypath = '/home/kunal/Desktop'
 os.chdir(mypath)
-print (os.listdir('.'))
+print(os.listdir('.'))
 files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 for file in files:
     li = list(file.split('.'))
@@ -14,4 +19,4 @@ for file in files:
     except FileExistsError:
         pass
     shutil.move(join(mypath, file), join(mypath, folder_name))
-print (os.listdir('.'))
+print(os.listdir('.'))
