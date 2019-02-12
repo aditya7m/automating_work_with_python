dab252aec94d60c0fff5fb0df52710aa7d7c6c84
sharma-kunal <kunalsharma6914@gmail.com>
2019-02-12 14:51:01 +0530

    Add changes done to a file for future references

--- desktopCleaner.py
+++ desktopCleaner.py
@@ -4,13 +4,22 @@
 # changing the mypath variable to your desired folder which you want to
 # clean up.
 
+# It's not a plain desktop cleaner, what it does is it checks for the file
+# extension name and collect all files with same extension to a folder named
+# like extension .
+
+# For e.g. files with pdf extension all in one folder named pdf, files with jpg
+# extension all in one folder named jpeg.
+
+
 import os
 import shutil
 from os.path import isfile, join
-mypath = '/home/<user-name>/Desktop'
+mypath = '/home/kunal/Desktop'
 os.chdir(mypath)
 print(os.listdir('.'))
 files = [f for f in os.listdir(mypath) if isfile(join(mypath, f))]
+f = open('changes.txt', 'w')    # all changes done to your path are shown in this file inside txt folder.
 for file in files:
     if file[0] == '.':
         continue
@@ -22,6 +31,8 @@ for file in files:
         pass
     try:
         shutil.move(join(mypath, file), join(mypath, folder_name))
+        f.write("%s moved to %s\n" % (file, folder_name))
     except shutil.Error:
-        pass
+        raise shutil.Error("File already exists in the folder")
+f.close()
 print(os.listdir('.'))
